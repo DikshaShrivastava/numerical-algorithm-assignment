a36c77c48412d0a67c92247e3fc8aaf7549b5cf7
DikshaShrivastava <diksha.shrivastava.cse14@itbhu.ac.in>
2015-03-31 00:14:41 +0530

    plotting included

--- Definite integral.py
+++ Definite integral.py
@@ -1,30 +1,52 @@
 
-# ques 5
-# usage: pass a function to be integrated,lower limit,upper limit,no of divisions
-# example:b.trapezoid(lambda x:x,2,3,50)
-import numpy
-
-class Integrate():
-
-  def trapezoid(self,f,a,b,n):
-    
-    self.x=numpy.linspace(a,b,n+1)
-    y=f(self.x)
-    s=y[0]+2.0*sum(y[1:n])+y[n]
-    h=float(b-a)/n
-    return s*h/2.0
-    
-   def simpson(self,f,a,b,n):
-        self.x=numpy.linspace(a,b,n+1)
-        y=f(self.x)
-    
-        h = float(self.x[1] - self.x[0])
-        n = len(self.x) - 1
-        if n % 2 == 1:
-            n -= 1
-        s = y[0] + y[n] + 4.0 * sum(y[1:-1:2]) + 2.0 * sum(y[2:-2:2])
-        return h * s / 3.0
-    
-    
-  
-    
+from matplotlib import pyplot as plt
+class Integrate:
+	def solve(self,order,coeffs,method):
+		def f(x):
+			sum=0
+			for i in range(order+1):
+				sum+=(coeffs[i]*(x**(order-i)))
+			return sum
+		if method=='trapezoid':
+			a=float(input('enter the lower limit of interval: '))
+			b=float(input('enter the upper limit of interval: '))
+			n=int((b-a)/0.001)
+			x_values=[a]
+			for i in range(1,n):
+				x_values.append(float(str(x_values[0]+(0.001*i))[:5]))
+			x_values.append(b)
+                        y_values=[f(x) for x in x_values]
+                        a=plt.plot(x_values,y_values);
+                        for j in range(1,n+1):
+                                points=[[x_values[j-1],0],[x_values[j-1],y_values[j-1]],[x_values[j],y_values[j]],[x_values[j],0]]
+                                polygon=plt.Polygon(points,closed=True,fill=None,edgecolor='r')
+                                a=plt.gca().add_patch(polygon)
+                        def trapezoid_solution(f,x_values,n):
+				sum=0
+				for i in range(1,n):
+					sum+=f(x_values[i])
+				sum*=2
+				sum+=(f(x_values[0])+f(x_values[n]))
+				ans=((x_values[n]-x_values[0])*sum)/(2*n)
+				return ans
+			return trapezoid_solution(f,x_values,n)
+		elif method=='simpson':
+			a=float(input('enter the lower limit of interval: '))
+			b=float(input('enter the upper limit of interval: '))
+			n=int((b-a)/0.0005) #0.0005 instead of 0.001 to ensure n is even
+			x_values=[a]
+			for i in range(1,n):
+				x_values.append(float(str(x_values[0]+(0.0005*i))[:5]))
+			x_values.append(b)
+			def simpson_solution(f,x_values,n):
+				sum1,sum2,sum=0,0,0
+				for i in range(1,n,2):
+					sum1+=f(x_values[i])
+				sum1*=4
+				for i in range(2,n,2):
+					sum2+=f(x_values[i])
+				sum2*=2
+				sum+=(f(x_values[0])+f(x_values[n])+sum1+sum2)
+				ans=((x_values[n]-x_values[0])*sum)/(3*n)
+				return ans
+			return simpson_solution(f,x_values,n)
